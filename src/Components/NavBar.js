099846b1d40c019d8681d1a7061532e3eb72ea88
Rene <racosta323@gmail.com>
2024-02-09 02:11:25 -0600

    rearranges button and nav links

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -19,58 +19,51 @@ function NavBar(){
                 bg="primary"
                 data-bs-theme="dark"
                 fixed="top"
+                expand="lg"
             >
-                <Container className='pt-2 m-auto'>
-                    <Stack>
-                        <Nav className='ms-auto'>
-                            <Nav.Link href="#" className="text-light fs-4 px-3">Home</Nav.Link>
-                            <Nav.Link href="#" className="text-light fs-4 px-3">About</Nav.Link>
-                            <Nav.Link href="#" className="text-light fs-4 px-3">Add a cat!</Nav.Link>
-                        </Nav>
-
-                        <Navbar.Brand href="#home" className='my-2'>
-                            <Row>
-                                <Col className="align-self-center" sm={2}>
-                                    <Image 
-                                    src="https://shorturl.at/pxDY6"
-                                    alt="UPDATE THIS"
-                                    roundedCircle
-                                    width={180}
-                                    height={180}
-                                    /> {' '}
-                                </Col>
-
-                                <Col className="align-self-center">
-                                    <h1 className='text-light'>Catspace</h1>
-                                    <p className="text-light">Where cats have profiles</p>
-                                </Col>
-
-                                <Col className='align-self-center'>
-                                    {/*FORM HERE*/}
-                                    <Form inline>
-                                        <Row className="justify-content-end">
-                                            <Col xs="auto">
-                                                <Form.Control
-                                                    type="text"
-                                                    placeholder="Search for a cat"
-                                                    className="mr-sm-2"
-                                                />
-                                            </Col>
-                                            <Col xs="auto">
-                                                <Button 
-                                                    type="submit"
-                                                    variant="light"
-                                                >Submit</Button>
-                                            </Col>
-                                        </Row>
-                                    </Form>
-                                </Col>
-                            </Row>
-                        </Navbar.Brand>
-                    </Stack>
-                    
-
-
+                <Container fluid>
+                    <Navbar.Brand href="#home" className='m-5'>
+                        <Row>
+                            <Col xs={3} md={2} className="align-self-center">
+                                <Image 
+                                src="https://shorturl.at/pxDY6"
+                                alt="UPDATE THIS"
+                                roundedCircle
+                                width={180}
+                                height={180}
+                                /> {' '}
+                            </Col>
+                            <Col className="align-self-center ms-5" xs={3} md={4}>
+                                <h1 className='text-light'>Catspace</h1>
+                                <p className="text-light">Where cats have profiles</p>
+                                <Nav className='ms-6 my-2 my-lg-0'>
+                                    <Nav.Link href="#" className="text-light fs-4 pe-3 ps-0">Home</Nav.Link>
+                                    <Nav.Link href="#" className="text-light fs-4 pe-3">About</Nav.Link>
+                                    <Nav.Link href="#" className="text-light fs-4">Add a cat!</Nav.Link>
+                                </Nav>
+                            </Col>
+                            <Col className="d-flex justify-content-end align-items-center ms-5 p-0" xs={12} md="auto">
+                                {/*FORM HERE*/}
+                                <Form className="d-flex">
+                                    <Row>
+                                        <Col xs="auto">
+                                            <Form.Control
+                                                type="text"
+                                                placeholder="Search for a cat"
+                                                className="mr-sm-2"
+                                            />
+                                        </Col>
+                                        <Col xs="auto" className='flex=grow-1'>
+                                            <Button 
+                                                type="submit"
+                                                variant="light"
+                                            >Submit</Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Col>
+                        </Row>
+                    </Navbar.Brand>
                 </Container>
             </Navbar>
         </>    
