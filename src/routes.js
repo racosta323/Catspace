dafaaa3652377c9acb6ed9dc4e9eb346a29d04e3
Rene <racosta323@gmail.com>
2024-02-11 12:04:10 -0600

    removes id from catprofile temporarily

--- src/routes.js
+++ src/routes.js
@@ -20,7 +20,7 @@ const routes = [
                 element: <About/>
             },
             {
-                path: "/profile:id",
+                path: "/profile",
                 element: <CatProfile/>
             },
             {
