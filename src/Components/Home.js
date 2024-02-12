dd9e9df83fa74ba60ed1d75527075dbb4419fff3
Rene <racosta323@gmail.com>
2024-02-12 10:23:38 -0600

    start homepage edits

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,83 @@
+import { Container, Col, Row, Image } from "react-bootstrap"
+
 function Home(){
-    return <p>hello</p>
+    
+    
+    return (
+        <Container>
+            <Row className="m-5"></Row>
+            <Row className="m-5">
+                <Col>
+                    <h3 className="fs-1">Behold</h3>
+                </Col>
+            </Row>
+
+            <Row>
+                
+                <Col>
+                    <Image
+                        src="https://www.rd.com/wp-content/uploads/2022/03/GettyImages-509629699-scaled-e1648763472476.jpg?fit=700,466"
+                        alt="something"
+                        roundedCircle
+                        className="home-image-size"
+                    />
+                </Col>
+
+                <Col>
+                    <Image
+                        src="https://www.rd.com/wp-content/uploads/2022/03/GettyImages-509629699-scaled-e1648763472476.jpg?fit=700,466"
+                        alt="something"
+                        roundedCircle
+                        className="home-image-size"
+                    />
+                </Col>
+
+                <Col>
+                    <Image
+                        src="https://www.rd.com/wp-content/uploads/2022/03/GettyImages-509629699-scaled-e1648763472476.jpg?fit=700,466"
+                        alt="something"
+                        roundedCircle
+                        className="home-image-size"
+                    />
+                </Col>
+
+                <Col>
+                    <Image
+                        src="https://www.rd.com/wp-content/uploads/2022/03/GettyImages-509629699-scaled-e1648763472476.jpg?fit=700,466"
+                        alt="something"
+                        roundedCircle
+                        className="home-image-size"
+                    />
+                </Col>
+
+                <Col>
+                    <Image
+                        src="https://www.rd.com/wp-content/uploads/2022/03/GettyImages-509629699-scaled-e1648763472476.jpg?fit=700,466"
+                        alt="something"
+                        roundedCircle
+                        className="home-image-size"
+                    />
+                </Col>
+                
+                <Col>
+                    <Image
+                        src="https://www.rd.com/wp-content/uploads/2022/03/GettyImages-509629699-scaled-e1648763472476.jpg?fit=700,466"
+                        alt="something"
+                        roundedCircle
+                        className="home-image-size"
+                    />
+                </Col>
+
+                
+       
+
+            </Row>
+       
+       
+       
+       
+       </Container>
+    )
 }
 
 export default Home
